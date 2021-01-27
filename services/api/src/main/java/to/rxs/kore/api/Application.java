3d22243b875372ba25f8e41e212efc6fd65eaa6e
Oskar Lang <oskar.risto.lang@gmail.com>
2021-01-27 23:37:18 +0100

    feat: add automatic configuration

--- services/api/src/main/java/to/rxs/kore/api/Application.java
+++ services/api/src/main/java/to/rxs/kore/api/Application.java
@@ -3,9 +3,11 @@ package to.rxs.kore.api;
 import lombok.extern.flogger.Flogger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import reactor.core.publisher.Hooks;
 
 @Flogger
+@EnableConfigurationProperties
 @SpringBootApplication
 public class Application {
 
