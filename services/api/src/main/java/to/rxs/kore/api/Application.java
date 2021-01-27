66a8a31354572276891b3179a3d84307e0a6044e
Oskar Lang <oskar.risto.lang@gmail.com>
2021-01-27 23:37:06 +0100

    feat: add reactor debug hooks

--- services/api/src/main/java/to/rxs/kore/api/Application.java
+++ services/api/src/main/java/to/rxs/kore/api/Application.java
@@ -3,12 +3,14 @@ package to.rxs.kore.api;
 import lombok.extern.flogger.Flogger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Hooks;
 
 @Flogger
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
+        Hooks.onOperatorDebug();
         SpringApplication.run(Application.class, args);
     }
 }
