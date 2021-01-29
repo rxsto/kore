46ddb319586b19874c7bdd0f2f781774060177ac
Oskar Lang <oskar.risto.lang@gmail.com>
2021-01-29 19:23:54 +0100

    chore: clean up and remove debug log

--- services/api/src/main/java/to/rxs/kore/api/Application.java
+++ services/api/src/main/java/to/rxs/kore/api/Application.java
@@ -4,12 +4,10 @@ import lombok.extern.flogger.Flogger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.ComponentScan;
 import reactor.core.publisher.Hooks;
 
 @Flogger
 @EnableConfigurationProperties
-//@ComponentScan("to.rxs")
 @SpringBootApplication
 public class Application {
 
