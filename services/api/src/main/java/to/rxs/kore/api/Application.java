b84a07ce4e9fd4e1767d520c0e982037dcdf80bc
Oskar Lang <oskar.risto.lang@gmail.com>
2021-01-29 02:57:15 +0100

    feat: enable component scanning

--- services/api/src/main/java/to/rxs/kore/api/Application.java
+++ services/api/src/main/java/to/rxs/kore/api/Application.java
@@ -4,10 +4,12 @@ import lombok.extern.flogger.Flogger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
 import reactor.core.publisher.Hooks;
 
 @Flogger
 @EnableConfigurationProperties
+@ComponentScan("to.rxs")
 @SpringBootApplication
 public class Application {
 
