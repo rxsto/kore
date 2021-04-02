1d3ee669df988fb5726ca537f52fc2abc2b480c3
Oskar Lang <oskar.risto.lang@gmail.com>
2021-04-02 09:14:07 +0200

    feat: migrate from GHCR to GCR

--- services/api/build.gradle.kts
+++ services/api/build.gradle.kts
@@ -47,12 +47,8 @@ jib {
     }
 
     to {
-        image = "ghcr.io/rxsto/kore-" + project.name
+        image = "gcr.io/rxs-to/kore-" + project.name
         tags = setOf(System.getenv("SEMAPHORE_GIT_SHA"))
-        auth {
-            username = "rxsto"
-            password = System.getenv("GITHUB_TOKEN")
-        }
     }
 
     container {
